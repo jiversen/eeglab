jiversen/eeglab

commit 6324d5acecef92b5dcbf703fd0639395520347d5

fixing .set and .fdt file incompatibility

functions/popfunc/pop_loadset.m
@@ -133,7 +133,18 @@ else
                 end
             end
             if isfield(TMPVAR, 'datfile') && ~isempty(TMPVAR.datfile)
-                TMPVAR.data = TMPVAR.datfile;
+                if exist(TMPVAR.datfile, 'file')
+                    TMPVAR.data = TMPVAR.datfile;
+                else
+                    warning('.fdt file not found, checking if .set contains data')
+                    TMPVAR = load('-mat', filename);
+                    if ~isnumeric(TMPVAR.data) && ~isempty(TMPVAR.data) 
+                        warning('.fdt file not found, but data found in .set EEGLAB file')
+                        TMPVAR.data = 'in set file';
+                    else
+                        warning('.fdt file not found, this EEGLAB file is missing raw data')
+                    end
+                end
             else
                 TMPVAR.data = 'in set file';
             end

eeglab.m
@@ -1039,7 +1039,7 @@ else
                     
                 % special case of subfolder for BIOSIG
                 % ------------------------------------
-                if ~isempty(findstr(lower(dircontent(index).name), 'biosig')) && isempty(findstr(lower(dircontent(index).name), 'biosigplot'))
+                if ~isempty(findstr(lower(dircontent(index).name), 'biosig')) && isempty(strfind(lower(dircontent(index).name), 'biosigplot'))
                     addpathifnotexist( fullfile(dircontent(index).folder, dircontent(index).name, 'biosig', 't200_FileAccess'), 'sopen.m');
                     addpathifnotexist( fullfile(dircontent(index).folder, dircontent(index).name, 'biosig', 't250_ArtifactPreProcessingQualityControl'), 'regress_eog.m' );
                     addpathifnotexist( fullfile(dircontent(index).folder, dircontent(index).name, 'biosig', 'doc'), 'DecimalFactors.txt');

commit dbecd90e3d00b0154f61c7a5d699ee220d933323

fix bug in pop_loadset with 'loadmode' 'info'
Bug introduced in commit 6324d5acecef92b5dcbf703fd0639395520347d5

functions/popfunc/pop_loadset.m
@@ -135,8 +135,15 @@ else
                     TMPVAR = rmfield(TMPVAR, 'data');
                 end
             end
+
+%             ### Description
+% 
+% The point of 'filemode' 'info' is to quickly load metadata without loading the data. 
+% 
+% The current version of pop_loadset (introduced in commit 6324d5acecef92b5dcbf703fd0639395520347d5) seems to care whether the data can be found and if the .fdt is not found it then loads the entire .set file again (including the data field) to check if the .set contains data. This is unnecessary as I'm not asking for some kind of consistency check, just to quickly load the metadata. If the set file did happen to contain data (single file mode) this would additionally defeat the purpose of 'info' mode by forcing to load the data.
+
             if isfield(TMPVAR, 'datfile') && ~isempty(TMPVAR.datfile)
-                if exist(TMPVAR.datfile, 'file')
+                if true || exist(TMPVAR.datfile, 'file') %JRI: FIXME: this check assumes .fdt in same directory but that's not always the case
                     TMPVAR.data = TMPVAR.datfile;
                 else
                     warning('.fdt file not found, checking if .set contains data')